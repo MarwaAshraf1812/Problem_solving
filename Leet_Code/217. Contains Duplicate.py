5780ae9b972dd455e57841b2a6a8bf7fad17f213
MarwaAshraf1812 <marashraf090@gmail.com>
2024-03-09 20:54:41 +0200

    Day1: reducing from O(n^2) to O(n)

---
+++ Leet_Code/217. Contains Duplicate.py
@@ -0,0 +1,15 @@
+"""
+    This implementation uses a set to efficiently keep track of 
+    unique elements encountered during the iteration. If a duplicate is found, 
+    the method returns True; otherwise, it returns False. The time complexity of this solution is O(n),
+    where n is the length of the input array nums, 
+    due to the constant-time average complexity of set operations like in and add.
+"""
+class Solution(object):
+    def containsDuplicate(self, nums):
+        un_set = set()
+        for i in nums:
+            if i in un_set: return True
+            else: un_set.add(i)
+        return False
+
