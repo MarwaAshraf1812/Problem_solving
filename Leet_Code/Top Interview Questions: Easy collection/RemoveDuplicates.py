4e40e467cd34a463096928f31d5384a6f16001b4
MarwaAshraf1812 <marashraf090@gmail.com>
2024-04-21 02:59:55 +0200

    updated on the answer

--- Leet_Code/Top Interview Questions: Easy collection/RemoveDuplicates.py
+++ Leet_Code/Top Interview Questions: Easy collection/RemoveDuplicates.py
@@ -4,12 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        j = 0
-        for i in range(0, len(nums)):
-            if nums[i] != nums[j]:
-                j += 1
-                nums[j] = nums[i]
-        return j + 1
+        # j = 0
+        # for i in range(0, len(nums)):
+        #     if nums[i] != nums[j]:
+        #         j += 1
+        #         nums[j] = nums[i]
+        # # return k  len of new array 
+        # return j + 1
+
+        """ 
+        set() removes duplicates but didnt return new array length
+        but we(eslam ana marwa :)) can do a hack to using set to remove duplicates
+        and then sort the set and itrate on it and replace the
+        items to the original array and return the length of the
+        array
+        """
+        unique = set(nums)
+        count = 0
+        unique = sorted(unique)
+        for i in unique:
+            nums[count] = i
+            count += 1
+        return count
 
 # 362 / 362 test cases passed.
 # Status: Accepted
