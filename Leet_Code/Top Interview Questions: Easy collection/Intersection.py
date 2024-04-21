5c7783f7a577956e6a4389f08a6e1fe2779d2f89
MarwaAshraf1812 <marashraf090@gmail.com>
2024-04-21 03:47:16 +0200

    intersection

--- Leet_Code/Top Interview Questions: Easy collection/Intersection.py
+++ Leet_Code/Top Interview Questions: Easy collection/Intersection.py
@@ -5,24 +5,19 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        dict_1 = {}
-        dict_2 = {}
-        for i in nums1:
-            if i not in dict_1:
-                dict_1[i] = 1
-            else:
-                dict_1[i] += 1
-        
-
-        """
         answer = []
         for i in range(0, len(nums1)):
             if nums1[i] in nums2:
                 answer.append(nums1[i])
                 nums2.remove(nums1[i])
         return answer
+
         """
-    
+        set_1 = Multiset(nums1)
+        set_2 = Multiset(nums2)
+        set_3 = set_1.intersection(set_2)
+        return set_3
+        """
 """
 57 / 57 test cases passed.
 Status: Accepted
