a0b7f762b94574bd09e06a047a388035f1f8def4
MarwaAshraf1812 <marashraf090@gmail.com>
2024-04-21 05:20:37 +0200

    logic tmam implementation no 5als

---
+++ Leet_Code/Top Interview Questions: Easy collection/RotateImage.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i,n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(n):
+            matrix[i].reverse()
+        return matrix
+"""
+21 / 21 test cases passed.
+Status: Accepted
+Runtime: 19 ms
+Memory Usage: 11.7 MB
+"""
